c94a90e1c99e313a3c4b3163cbe53080fe40bd4d
yael16yo <75533817+yael16yo@users.noreply.github.com>
2024-09-19 12:03:10 +0200

    update(multer config : port)

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const routesBooks = require('./routes/routesBooks.js');
-const routesUsers = require('./routes/routesUsers.js');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const routesBooks = require("./routes/routesBooks.js");
+const routesUsers = require("./routes/routesUsers.js");
 
 const mongoUri = process.env.MONGO_URI;
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 app.use(express.json());
-app.use('/api/books', routesBooks);
-app.use('/api/auth', routesUsers);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/api/books", routesBooks);
+app.use("/api/auth", routesUsers);
+app.use("/images", express.static(path.join(__dirname, "images")));
 module.exports = app;
--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,49 +1,55 @@
-const express = require('express');
-const multer = require('multer');
-const sharp = require('sharp');
+const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 
 const opti = express();
 const storage = multer.memoryStorage();
-const upload = multer({storage});
+const upload = multer({ storage });
 
 opti.use(express.static("./images"));
 
-opti.post('/', upload.single('image'), async (req, res, callback) => {
-  const {buffer, originalname} = req.file;
-  
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    const length = 16;
-    let randomstring = "";
-    for (i = 0; i < length; i++) {
-      randomstring += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
+opti.post("/", upload.single("image"), async (req, res, callback) => {
+  const { buffer, originalname } = req.file;
 
-  const modifiedname = originalname.replaceAll(' ', '');
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  const length = 16;
+  let randomstring = "";
+  for (i = 0; i < length; i++) {
+    randomstring += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
+  const modifiedname = originalname.replaceAll(" ", "");
   const ref = `${randomstring}${modifiedname}.webp`;
   await sharp(buffer)
-  .webp({ quality: 30 })
-  .toFile('./images/' + ref);
-  const fullLink = `http://localhost:3000/images/${ref}`;
+    .webp({ quality: 30 })
+    .toFile("./images/" + ref);
+  const fullLink = `http://localhost:4000/images/${ref}`;
   req.fullLink = fullLink;
   callback(null, fullLink);
 });
 
-opti.put('/:id', upload.single('image'), async (req, res, callback) => {
-  const {buffer, originalname} = req.file;
-  
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    const length = 16;
-    let randomstring = "";
-    for (i = 0; i < length; i++) {
-      randomstring += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
+opti.put("/:id", upload.single("image"), async (req, res, callback) => {
+  const { buffer, originalname } = req.file;
 
-  const modifiedname = originalname.replaceAll(' ', '');
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  const length = 16;
+  let randomstring = "";
+  for (i = 0; i < length; i++) {
+    randomstring += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
+  const modifiedname = originalname.replaceAll(" ", "");
   const ref = `${randomstring}${modifiedname}.webp`;
   await sharp(buffer)
-  .webp({ quality: 30 })
-  .toFile('./images/' + ref);
-  const fullLink = `http://localhost:3000/images/${ref}`;
+    .webp({ quality: 30 })
+    .toFile("./images/" + ref);
+  const fullLink = `http://localhost:4000/images/${ref}`;
   req.fullLink = fullLink;
   callback(null, fullLink);
 });
